f03aace33d1a3f76526d66098912ad23fb58f20a
黃宣揚 0513342 <ckscks038038.iem05@o365.nctu.edu.tw>
2021-11-24 19:25:45 +0800

    add jeff huang

---
+++ module.js
@@ -0,0 +1,5 @@
+const xyz = require('./people'); //require load other file in here.
+
+console.log(xyz.ag);
+console.log(xyz.ag);
+console.log(xyz.peo);
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['yoshi', 'ryu', 'chun-li', 'mario'];
+const ages = [20, 25, 30, 35];
+console.log(people);
+
+module.exports = {
+    peo: people, ag: ages
+};//the thing you want to export to module.js as the return value
